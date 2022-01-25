f2d12678d34b0d3901848c6938384494a4ead482
Junhao Ou <92162461+Leonardou0810@users.noreply.github.com>
2022-01-25 10:47:33 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,65 @@
 import platform
+import socket
+import multiprocessing
+import psutil
 
 def showinfo(tip, info):
-    print("{}:{}".format(tip,info))
+    print("{}: {}".format(tip,info))
  
  
-showinfo("操作系统及版本信息",platform.platform())
-showinfo('获取系统版本号',platform.version())
-showinfo('获取系统名称', platform.system())
-showinfo('系统位数', platform.architecture())
-showinfo('计算机类型', platform.machine())
-showinfo('计算机名称', platform.node())
-showinfo('处理器类型', platform.processor())
-showinfo('计算机相关信息', platform.uname())
+showinfo("Name of machine", socket.gethostname())
+showinfo("Operating system name",platform.system())
+showinfo('Operating system version',platform.version())
+showinfo("CPUs", multiprocessing.cpu_count())
+showinfo("Amount of memory", psutil.virtual_memory().total)
+showinfo("IP address", socket.gethostbyname(socket.gethostname()))
 
+import timeit
+import math
+import itertools
+
+def bench_pidigits(ndigits=1000, loops=100):
+
+    def calc_ndigits(n):
+    # Adapted from code on http://shootout.alioth.debian.org/
+        def gen_x():
+            return map(lambda k: (k, 4*k + 2, 0, 2*k + 1), itertools.count(1))
+
+    def compose(a, b):
+        aq, ar, as_, at = a
+        bq, br, bs, bt = b
+        return (aq * bq,
+                aq * br + ar * bt,
+                as_ * bq + at * bs,
+                as_ * br + at * bt)
+
+    def extract(z, j):
+        q, r, s, t = z
+        return (q*j + r) // (s*j + t)
+
+    def pi_digits():
+        z = (1, 0, 0, 1)
+        x = gen_x()
+        while 1:
+            y = extract(z, 3)
+            while y != extract(z, 4):
+                z = compose(z, next(x))
+                y = extract(z, 3)
+            z = compose((10, -10*y, 0, 1), z)
+            yield y
+
+        return list(itertools.islice(pi_digits(), n))
+
+    for _ in range(loops):
+        calc_ndigits(ndigits)
+    #print(’Pi:’, ’’.join(map(str, calc_ndigits(ndigits))))
+    return
+
+    #return perf.perf_counter() - t0
+
+if __name__ == '__main__':
+    t_default = 6.388216104
+    start_time = timeit.default_timer()
+    bench_pidigits(ndigits=1000, loops=100)
+    elapsed_time = timeit.default_timer() - start_time
+    print('relative elapsed:', elapsed_time/t_default)
